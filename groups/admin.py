e708337e205b944c50409d4a400aaf14c465636f
jainraunak <jainraunak846@gmail.com>
2019-07-07 15:17:52 +0530

    First commit

---
+++ groups/admin.py
@@ -0,0 +1,12 @@
+# Register your models here.
+from django.contrib import admin
+
+from . import models
+
+
+class GroupMemberInline(admin.TabularInline):
+    model = models.GroupMember
+
+
+
+admin.site.register(models.Group)
